013f3fc10a82f373677b21734336ea351f3a5d44
Harsh0478 <harshrathod7798@gmail.come>
2026-02-14 14:07:16 +0530

    Auth Initilized

---
+++ Backend/src/config/inggest.js
@@ -0,0 +1,45 @@
+import { Inngest } from "inngest";
+import { connectDB } from "./connectDB.js";
+import User from "../models/UsersModel.js";
+
+// Create a client to send and receive events
+export const inngest = new Inngest({ id: "SLAK" });
+
+const syncUser = inngest.createFunction(
+  { id: "Sync-User" },
+  { event: "clerk/user.created" },
+  async ({ event }) => {
+    connectDB();
+
+    const { id, email_addresses, first_name, last_name, image_url } =
+      event.data;
+
+    const newUser = {
+      clerkId: id,
+      email: email_addresses[0].email_address,
+      name: `${first_name} ${last_name}`,
+      imageUrl: image_url,
+    };
+
+    User.create(newUser);
+
+    // TODO: Add error handling and logging
+  },
+);
+
+const deleteUserFromDB = inngest.createFunction(
+  { id: "Delete_User" },
+  { event: "clerk/user.deleted" },
+
+  async ({ event }) => {
+    connectDB();
+
+    const { id } = event.data;
+    await User.deleteOne({ clerkId: id });
+
+    // TODO: Add error handling and logging
+  },
+);
+
+// Create an empty array where we'll export future Inngest functions
+export const functions = [syncUser, deleteUserFromDB];
---
+++ Backend/src/models/UsersModel.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    clerkId: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    imageUrl: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true },
+);
+
+export default mongoose.model("User", userSchema);
